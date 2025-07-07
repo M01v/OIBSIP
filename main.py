fd6f031f40122d75c19a08b994b7e000e1029c73
M01v <mvwork02@gmail.com>
2025-07-07 17:03:18 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import random
-chars = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
-length=int(input("Enter length:"))
-password=""
-for a in range(length):
-    password+=random.choice(chars)
-print(password)
